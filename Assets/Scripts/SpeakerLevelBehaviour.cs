84f7bb58cf0a98a48e958896c952a8f7642b6174
Matthijs <matthijs07g@gmail.com>
2024-05-13 15:02:11 +0200

    mechanics toetslevel af

---
+++ Assets/Scripts/SpeakerLevelBehaviour.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeakerLevelBehaviour : MonoBehaviour
+{
+
+    public int goal;
+    int streak = 0;
+    int answer;
+
+    string selectedAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectSound();
+        playSound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(streak == goal)
+        {
+            Debug.Log("level completed");
+        }
+    }
+
+
+    public void playSound()
+    {
+        FindObjectOfType<AudioManager>().Play(selectedAudio);
+    }
+
+    public void selectSound()
+    {
+        float choice = Random.Range(1, 9);
+        Debug.Log(choice);
+        if(choice == 1 || choice == 5 )
+        {
+            selectedAudio = "1";
+            answer = 1;
+        }
+        if(choice == 2 || choice == 6 )
+        {
+            selectedAudio = "2";
+            answer = 2;
+        }
+        if (choice == 3 || choice == 7)
+        {
+            selectedAudio = "3";
+            answer = 3;
+        }
+        if (choice == 4 || choice == 8)
+        {
+            selectedAudio = "4";
+            answer = 4;
+        }
+    }
+
+    public void checkAnswer(int choice)
+    {
+
+        if (choice == answer)
+        {
+            Debug.Log("Correct");
+            streak += 1;
+            if (streak != goal)
+            {
+                selectSound();
+                playSound();
+            }
+
+        }
+        else if (choice != answer)
+        {
+            Debug.Log("Fout");
+            streak = 0;
+        }
+
+    }
+}
