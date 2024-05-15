60dd08b1895975876d1d869208cf21a9178e789c
Liam <lcn.gevaerts@student.avans.nl>
2024-05-15 12:14:55 +0200

    Fixed animation

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -211,7 +211,7 @@ public class GridManager : MonoBehaviour
 
                 ClearAllMatches();
                 StartCoroutine(Fill());
-               level.OnMove();
+                level.OnMove();
             }
             else
             {
